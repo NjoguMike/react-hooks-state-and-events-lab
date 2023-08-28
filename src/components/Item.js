2c732bf5ea76a65657eb5c7cc253194b67f8ae1c
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-08-28 22:29:32 +0300

    Complete State Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+const [cart , setCart] = useState("")
+
+function handleCart(event){
+  const item = event.target.value
+
+  setCart("in-cart")
+}
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{cart === "" ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
